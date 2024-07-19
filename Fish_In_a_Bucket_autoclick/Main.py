0efce2ad6704fad0b8bdf44c9a523cf276427ad0
Fish_in_a_Bukcet_ <pyramid2949oia4ibdl@outlook.com>
2024-07-19 13:04:21 -0300

    atualização
    janelas / botão voltar (apaga a janela)

--- Fish_In_a_Bucket_autoclick/Main.py
+++ Fish_In_a_Bucket_autoclick/Main.py
@@ -1,20 +1,21 @@
+import tkinter
 from tkinter import *
 ###########################
 
 root = Tk()
 
-class Application(): #responsavel por abrir a janela (Inicio)
+class Application():  #responsavel por abrir a janela - Inicio
     def __init__(self):
         self.root = root
-        self.Screen() # puxa a função Screen()
-        self.widgets() # puxa a função widgets()
+        self.Screen() #puxa a função Screen()
+        self.widgets() #puxa a função widgets()
+
 
         root.mainloop() #mantem a janela aberta
 
-
-    # -# ############# Autoclicker ############## #- #
+        # -# ############# Autoclicker ############## #- #
     def autoclick(self):
-        autoclick = Tk()
+        autoclick = tkinter.Toplevel()
 
         autoclick.title("Autoclicker")
         autoclick.minsize(390, 282)
@@ -22,8 +23,38 @@ class Application(): #responsavel por abrir a janela (Inicio)
 
         autoclick.configure(background='#023059')
 
-        root.destroy()
+        Button(autoclick, text='[Voltar]', command=autoclick.destroy).place(relx=0.02, rely=0.02, relwidth=0.2, relheight=0.1)
+        #--#
+
     # -# ############# Autoclicker ############## #- #
+
+    # -# ############# Auto-Type ############## #- #
+    def autotype(self):
+        autotype = Tk()
+
+        autotype.title("Auto-Typer")
+        autotype.minsize(390, 282)
+        autotype.resizable(False, False)
+
+        autotype.configure(background='#023059')
+
+        Button(autotype, text='[Voltar]', command=autotype.destroy).place(relx=0.02, rely=0.02, relwidth=0.2, relheight=0.1)
+    # -# ############# Auto-Type ############## #- #
+
+# -# ############# Automacao ############## #- #
+    def automation(self):
+        automation = Tk()
+
+        automation.title("Automação")
+        automation.minsize(300, 200)
+        automation.resizable(False, False)
+
+        automation.configure(background='#030833')
+
+        Button(automation, text='[Voltar]', command=automation.destroy).place(relx=0.02, rely=0.02, relwidth=0.2, relheight=0.1)
+        #--#
+
+# -# ############# Automacao ############## #- #
 
     def Screen(self): # tamanho da tela ; Titulo ; limite de tela ; Cor de Fundo ;
         self.root.title("Fish-In-a-Bucket => Autoclick")
@@ -37,22 +68,21 @@ class Application(): #responsavel por abrir a janela (Inicio)
         self.lb_click = Label(text="leva para a tela de Autoclick")
         self.lb_click.place(relx=0.4, rely=0.02, relwidth=0.5, relheight=0.1)
 
-        self.bt_CLICK = Button(root, text='[Tela] AUTOCLICK', command=self.autoclick) #
+        self.bt_CLICK = Button(root, text='[Tela] AUTOCLICK', command=self.autoclick)#
         self.bt_CLICK.place(relx=0.02, rely=0.02, relwidth=0.4, relheight=0.1)
 
 
         self.lb_click = Label(text="leva para a tela de Auto-Typer")
         self.lb_click.place(relx=0.4, rely=0.15, relwidth=0.5, relheight=0.1)
 
-        self.bt_typer = Button(text='[Tela] AUTO-TYPER')
-        self.bt_typer.place(relx=0.02, rely=0.15, relwidth=0.4, relheight=0.1) #
+        self.bt_typer = Button(root, text='[Tela] AUTO-TYPER', command=self.autotype)#
+        self.bt_typer.place(relx=0.02, rely=0.15, relwidth=0.4, relheight=0.1)
 
         self.lb_automation = Label(text="leva para a tela de Automação")
-        self.lb_automation.place(relx=0.4, rely=0.28, relwidth=0.5, relheight=0.1) #
+        self.lb_automation.place(relx=0.4, rely=0.28, relwidth=0.5, relheight=0.1)
 
-        self.bt_automation = Button(text='[Tela] Automação')
+        self.bt_automation = Button(root, text='[Tela] Automação', command=self.automation)#
         self.bt_automation.place(relx=0.02, rely=0.28, relwidth=0.4, relheight=0.1)
-
 
 
 
